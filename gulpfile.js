ff3a179d172577d438eb325e7367696edcc5b11b
Tim <32385143+arcticstormx@users.noreply.github.com>
2020-05-17 20:30:43 +0300

    Add new gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,37 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = requrie("gulp-webp");
+var svgstore = require("gulp-svgstore");
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"))
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+});
+
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}).
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"))
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -17,6 +48,9 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipi(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
