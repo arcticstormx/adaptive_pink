12f1cac9ebd0e5f87be2501d61096716a1fb2a0f
Timur <sol485@outlook.com>
2020-05-27 18:47:22 +0300

    Still working on js

--- source/js/price-table.js
+++ source/js/price-table.js
@@ -2,41 +2,55 @@
 (function () {
   const priceTable = document.querySelector(".price-table");
   const packagesControls = document.querySelector(".packages__controls");
-  const controlsToggles = Array.from(packagesControls.querySelectorAll('.slider-controls__button'));
+  let controlsToggles = Array.from(packagesControls.querySelectorAll('.controls__toggle'));
 
 
   if (priceTable) {
-    packagesControls.addEventListener("click", handleButtonClick);
+    controlsToggles.forEach(el => {
+
+    }).addEventListener("click", handleButtonClick);
   }
 
-
-
-  function handleButtonClick(evt) {
-    debugger;
-    if (!evt.target.classList.contains("controls__toggle--active")) {
-
-      let activeButtonIndex = controlsToggles.indexOf(controlsToggles.find(el => {
-        return el.classList.contains("controls__toggle--active");
-      }));
-      console.log(activeButtonIndex);
-      let newActiveButtonIndex = controlsToggles.indexOf(evt.target);
-      console.log(newActiveButtonIndex);
-
-      controlsToggles[activeButtonIndex].classList.remove("controls__toggle--active");
-      controlsToggles[newActiveButtonIndex].classList.add("controls__toggle--active");
-
-      switch (newActiveButtonIndex) {
-        case 0:
-          priceTable.classList.add("packages__table--left");
-          break;
-        case 2:
-          priceTable.classList.add("packages__table--right");
-          break;
-        default:
-          break;
-      }
-    }
+function handleButtonClick(evt) {
+  if (!evt.target.classList.contains("controls__toggle--active")) {
+    let a = controlsToggles.findIndex(item => item.classList.contains("controls__toggle--active"));
+    controlsToggles[a].classList.remove("controls__toggle--active");
+    evt.target.classList.add("controls__toggle--active");
+    // let active = packagesControls.querySelector(".controls__toggle--active");
+    console.log("Im in!");
+    console.log("Active btn index" + a);
+    console.log(packagesControls);
+    console.log(controlsToggles);
 
   }
+}
+
+  // function handleButtonClick(evt) {
+  //   debugger;
+  //   if (!evt.target.classList.contains("controls__toggle--active")) {
+
+  //     let activeButtonIndex = controlsToggles.indexOf(controlsToggles.find(el => {
+  //       return el.classList.contains("controls__toggle--active");
+  //     }));
+  //     console.log(activeButtonIndex);
+  //     let newActiveButtonIndex = controlsToggles.indexOf(evt.target);
+  //     console.log(newActiveButtonIndex);
+
+  //     controlsToggles[activeButtonIndex].classList.remove("controls__toggle--active");
+  //     controlsToggles[newActiveButtonIndex].classList.add("controls__toggle--active");
+
+  //     switch (newActiveButtonIndex) {
+  //       case 0:
+  //         priceTable.classList.add("packages__table--left");
+  //         break;
+  //       case 2:
+  //         priceTable.classList.add("packages__table--right");
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   }
+
+  // }
 
 })();
