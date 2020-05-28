8bf863c4bb81a9aa4e3cce12feaec137d3e21236
Tim <32385143+arcticstormx@users.noreply.github.com>
2020-05-28 08:02:47 +0300

    Still making table

--- source/js/price-table.js
+++ source/js/price-table.js
@@ -6,19 +6,29 @@
 
 
   if (priceTable) {
-    controlsToggles.forEach(el => {
-
-    }).addEventListener("click", handleButtonClick);
+    controlsToggles.forEach(el => el.addEventListener("click", handleButtonClick);
   }
 
 function handleButtonClick(evt) {
   if (!evt.target.classList.contains("controls__toggle--active")) {
-    let a = controlsToggles.findIndex(item => item.classList.contains("controls__toggle--active"));
-    controlsToggles[a].classList.remove("controls__toggle--active");
+    let idx = controlsToggles.findIndex(item => item.classList.contains("controls__toggle--active"));
+    controlsToggles[idx].classList.remove("controls__toggle--active");
     evt.target.classList.add("controls__toggle--active");
+    switch idx {
+      case 0:
+        priceTable.classList.remove("packages__table--right");
+        priceTable.classList.add("packages__table--left");
+        break;
+      case 2:
+        priceTable.classList.remove("packages__table--left");
+        priceTable.classList.add("packages__table--right");
+        break;
+      default:
+        break;
+    }
     // let active = packagesControls.querySelector(".controls__toggle--active");
     console.log("Im in!");
-    console.log("Active btn index" + a);
+    console.log("Active btn index" + idx);
     console.log(packagesControls);
     console.log(controlsToggles);
 
